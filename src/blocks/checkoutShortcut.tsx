035bd5cbe2f3ff70244ade70d6c0dc9a3fada44a
Ricardo Camilo | Programador | React & Vue.js | Frontend <39412058+ricardo-camilo-programador-frontend-web@users.noreply.github.com>
2025-02-13 13:30:39 -0300

    Add conditional rendering for checkout shortcut based on cart items

--- src/blocks/checkoutShortcut.tsx
+++ src/blocks/checkoutShortcut.tsx
@@ -14,18 +14,20 @@ export default function CheckoutShortcut({ className }: CheckoutShortcutProps) {
   const formatCurrency = useFormatCurrency();
 
   return (
-    <div className={`flex items-center justify-center w-full bottom-12 -mt-6 ${className}`}>
-    <Link
-      href={`/checkout?step=shipping`}
-      className="bg-white !text-black px-4 py-2 w-full grid grid-cols-2 gap-2 border border-white/20 rounded-lg hover:!bg-white/70 transition-all duration-300 text-center"
-    >
-      <span className="font-bold">
-        {formatCurrency(totalOnStore)}&nbsp;
-      </span>
-      <span className="text-md font-bold">
-        {translateCart('checkout')}
-      </span>
-    </Link>
-  </div>
+    cartItems && cartItems.length > 0 && (
+      <div className={`flex items-center justify-center w-full bottom-12 -mt-6 ${className}`}>
+        <Link
+          href={`/checkout?step=shipping`}
+          className="bg-white !text-black px-4 py-2 w-full grid grid-cols-2 gap-2 border border-white/20 rounded-lg hover:!bg-white/70 transition-all duration-300 text-center"
+        >
+          <span className="font-bold">
+            {formatCurrency(totalOnStore)}&nbsp;
+          </span>
+          <span className="text-md font-bold">
+            {translateCart('checkout')}
+          </span>
+        </Link>
+      </div>
+    )
   )
 }
