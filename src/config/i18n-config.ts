428de683060c4e25a760c08da71be47fcb6859dc
Ricardo Camilo | Programador | React & Vue.js | Frontend <39412058+ricardo-camilo-programador-frontend-web@users.noreply.github.com>
2025-02-13 13:25:25 -0300

    Update locales and add locale and currency type definitions

--- src/config/i18n-config.ts
+++ src/config/i18n-config.ts
@@ -1,5 +1,5 @@
-export const locales = ['en', 'br', 'hi', 'jp', 'de', 'ar', 'pl'] as const;
-export const defaultLocale = 'pt' as const;
+export const locales = ['en-US', 'pt-BR', 'hi', 'jp', 'de', 'ar', 'pl'] as const;
+export const defaultLocale = 'en-US' as const;
 
 export type Locale = (typeof locales)[number];
 
@@ -12,3 +12,24 @@ export const isRTL = (locale: Locale): boolean => {
 export const getDirection = (locale: Locale): boolean => {
   return isRTL(locale);
 };
+
+export type LocaleType =
+| 'pt-BR'
+| 'en-US'
+| 'hi-IN'
+| 'ja-JP'
+| 'de-DE'
+| 'ar-SA'
+| 'pl-PL'
+| undefined
+
+
+export type CurrencyType =
+| 'BRL'
+| 'USD'
+| 'INR'
+| 'JPY'
+| 'EUR'
+| 'PLN'
+| undefined
+
