0b6fc3c625fed2db43ccd4c7cbb034cba74fbfec
Ricardo Camilo | Programador | React & Vue.js | Frontend <39412058+ricardo-camilo-programador-frontend-web@users.noreply.github.com>
2025-02-13 22:20:07 -0300

    Add bird singing ambient sound to sound list

--- src/static/ambientSounds.ts
+++ src/static/ambientSounds.ts
@@ -3,4 +3,8 @@ export const ambientSounds = [
     url: "/sounds/mixkit-morning-sound-in-a-garden-2464.mp3",
     altText: "Morning in a garden - mixkit",
   },
+  {
+    url: "/sounds/mixkit-bird-singing-and-flapping-wings-2432.wav",
+    altText: "Bird singing and flapping wings - mixkit",
+  },
 ];
