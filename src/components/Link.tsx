2074a80036212df215b87e0be033424ac0bfa7b4
Ricardo Camilo | Programador | React & Vue.js | Frontend <39412058+ricardo-camilo-programador-frontend-web@users.noreply.github.com>
2025-03-14 12:29:34 -0300

    removed link animation

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -1,9 +1,6 @@
-'use client';
-
 import type { FC } from 'react';
 import { ReactNode } from 'react';
 import Link, { LinkProps as NextLinkProps } from 'next/link';
-import { useRouter } from 'next/navigation';
 
 export interface LinkProps extends NextLinkProps {
   className?: string;
@@ -17,8 +14,6 @@ export interface LinkProps extends NextLinkProps {
   children: ReactNode;
 }
 
-const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
-
 const LinkComponent: FC<LinkProps> = ({
   children,
   href,
@@ -30,35 +25,8 @@ const LinkComponent: FC<LinkProps> = ({
   animate = true,
   ...props
 }: LinkProps) => {
-  const router = useRouter();
-
-  const handleTransition = async (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
-    if (!router) return;
-
-    e.preventDefault();
-
-    const sleepTime = 1000;
-    const body = document.querySelector('body');
-
-    if (body && animate) {
-      body.classList.add('page-transition');
-
-      await sleep(sleepTime);
-    }
-
-
-    await router.push(href);
-
-    if (body && animate) {
-      await sleep(sleepTime);
-
-      body.classList.remove('page-transition');
-    }
-  };
-
   return (
     <Link
-      onClick={handleTransition}
       href={href}
       target={externalLink ? '_blank' : '_self'}
       className={` hover:decoration-white active:text-white'} ${withUnderline && 'animate-underline'} ${className}`}
